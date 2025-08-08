aa1ec2bd270167159fa59b5e06570d3dce0fd978
Carlos Tapia Rueda <95229384+ctapiarueda@users.noreply.github.com>
2025-08-08 09:39:52 -0300

    Add files via upload

---
+++ query_vw_gra_RecReclamos_Cloaca.sql
@@ -0,0 +1,27 @@
+CREATE VIEW [dbo].[vw_gra_RecReclamos_Cloaca] AS
+SELECT  g.[RecId]
+      ,g.[TavNombre]
+      ,g.[RecNombre]
+      ,g.[SmoId]
+      ,g.[SmoNombre]
+      ,g.[RecDiametro]
+      ,g.[TmvId]
+      ,g.[RecObservaciones]
+      ,g.[RecX]
+      ,g.[RecY]
+      ,g.[FechaRecibido]
+      ,g.[FechaGrabado]
+      ,g.[FechaConfirmado]
+      ,g.[FechaSuprimido]
+	  ,geometry::Point(g.RecX, g.RecY, 22192) AS geom
+      ,c.[MorId]
+      ,c.[MorNombre]
+      ,c.[UbrLocalidad]
+      ,c.[DepId]
+  FROM [Jelu].[dbo].[vwRecReclamosConsultaGeneral] g
+  INNER JOIN [Jelu].[dbo].[vwRecReclamosCompletos] c
+    ON g.RecId = c.RecId
+  WHERE (g.[RecX]>2000000 AND g.[RecY]>6000000 AND
+  c.MorId =2)
+
+
