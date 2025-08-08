7e885b0a1beffd26c52717dbd62787d8e27557bb
Carlos Tapia Rueda <95229384+ctapiarueda@users.noreply.github.com>
2025-08-08 09:39:09 -0300

    Add files via upload

---
+++ query_vw_gra_odt_cloaca.sql
@@ -0,0 +1,18 @@
+
+CREATE   VIEW [dbo].[vw_gra_odt_cloaca] AS
+  SELECT [OdtId],
+          [MorId],
+          [MorNombre],
+		  [SmoNombre],
+		  [TeoNombre],
+		  [EsoFechaHoraOriginada] AS FechaOriginada,
+		  [EsoFechaHoraIniciada] AS FechaIniciada,
+		  [EsoFechaHoraFinalizada] AS FechaFinalizada,
+		  [DepId],
+		  [DepNombre] AS Departamento,
+		  GEOMETRY::Point(OdtX,OdtY,22192) AS geom
+  FROM [Jelu].[dbo].[vwOdtCompleta]
+  WHERE ([OdtX]>2000000 AND [OdtY]>6000000 AND MorId=2)
+
+
+
